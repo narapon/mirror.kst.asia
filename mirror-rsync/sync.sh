f5000847b5dd28953568ce1952ca84972adf4e8e
narapon <tanlikang11@gmail.com>
2023-12-26 20:41:24 +0000

    long due update

---
+++ mirror-rsync/sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# execute as a sync script
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo "Source or destination not set"
+else
+    echo "Beginning sync for $1"
+    echo "Syncing to $2"
+    rsync --recursive \
+        --links \
+        --hard-links \
+        --times \
+        --human-readable \
+        --chmod=a+rX \
+        --chmod=ug+w \
+        --safe-links \
+        --delete-delay \
+        --delete \
+        --progress \
+        "$1" "$2"
+fi
